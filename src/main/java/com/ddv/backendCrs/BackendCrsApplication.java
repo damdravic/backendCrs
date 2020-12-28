8c313be157599b6759757a63094d3a69378968db
Dragos <damian.dragosv@gmail.com>
2020-12-28 17:45:15 +0200

    Initial commit:basic structure

---
+++ src/main/java/com/ddv/backendCrs/BackendCrsApplication.java
@@ -0,0 +1,13 @@
+package com.ddv.backendCrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddv/backendCrs/BackendCrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddv.backendCrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
